3ad07eece25aa455eab34d193c8887a039f6b246
Vince-arch <vincekimani006@gmail.com>
2024-05-13 11:34:29 +0300

    adding card changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Card from '@mui/joy/Card';
-import items from '../Cakedata.json'
+import data from '../Cakedata.json'
 
 
 const Cards = () => {
   return (
-    <div>
-        <div name='card-container' className='flex bg-blue-400 h-42 w-96 m-4 outline'>
-          <div name="image"  className='rounded-full bg-white h-32 w-32 m-1'>  </div>
-          <div name="content" className="flex items-center justify-center m-14">Forest cake</div>
-          <div name="price" className="flex justify-end top-0">ksh.100</div>
-
+    <div className=' flex flex-col lg:flex-row lg:flex-wrap'>
+        {data.map(item =>(
+        <div name='card-container' className='flex  h-42 w-96 m-4 outline ' key={item.id}>
+          <img name="image"  className='rounded-full h-32 w-32 m-1' src={item.image} />
+          <div name="content" className="flex items-center justify-center m-10">{item.content}</div>
+          <div name="price" className="flex justify-end top-0 ">Ksh.{item.price}</div>
         </div>
+        ))}
     </div>
   )
 }
