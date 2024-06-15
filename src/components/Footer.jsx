02cf0eeec670961c63df64e546763e3b7fab3bc0
Vince-arch <vincekimani006@gmail.com>
2024-06-15 14:38:14 +0300

    fixing footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-4 h-32 bg-base-300 w-full text-base-content">
+      <footer className="footer footer-center p-4 h-32 bg-base-300 w-screen text-base-content">
           <aside>
           <p>Copyright © 2024 - All right reserved by Cake Planet</p>
         </aside>
